7701349fa2b24c48d35e29aaf26ec47af2538a21
v-shevchenko <mail.shevchenko.v@gmail.com>
2016-07-15 14:47:02 +0300

    1@11

--- src/Device.java
+++ src/Device.java
@@ -13,6 +13,27 @@ public class Device {
         this.serialNumber = serialNumber;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Device)) return false;
+
+        Device device = (Device) o;
+
+        if (Float.compare(device.price, price) != 0) return false;
+        if (!manufacturer.equals(device.manufacturer)) return false;
+        return serialNumber.equals(device.serialNumber);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = manufacturer.hashCode();
+        result = 31 * result + (price != +0.0f ? Float.floatToIntBits(price) : 0);
+        result = 31 * result + serialNumber.hashCode();
+        return result;
+    }
+
     public String getManufacturer() {
         return manufacturer;
     }
--- src/EthernetAdapter.java
+++ src/EthernetAdapter.java
@@ -11,6 +11,27 @@ public class EthernetAdapter extends Device {
         this.mac = mac;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof EthernetAdapter)) return false;
+        if (!super.equals(o)) return false;
+
+        EthernetAdapter that = (EthernetAdapter) o;
+
+        if (speed != that.speed) return false;
+        return mac.equals(that.mac);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + speed;
+        result = 31 * result + mac.hashCode();
+        return result;
+    }
+
     public int getSpeed() {
         return speed;
     }
